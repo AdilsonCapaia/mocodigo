18e0ae2aff3d33b995f6abc6baebb70b774029d6
AdilsonCapaia <adilsoncapaia@hotmail.com>
2020-05-24 15:52:25 +0200

    Adicionado novo template HTML

--- confcodigo/__init__.py
+++ confcodigo/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 from . import conferencia
 from .blueprints.admin import CCAdmin
 CCApp = Flask(__name__)
 
 CCApp.register_blueprint(conferencia.CCConferencia)
 CCApp.register_blueprint(CCAdmin)
+
+@CCApp.route('/')
+def inicio():
+    return render_template('inicio.html')
